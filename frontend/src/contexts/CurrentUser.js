c63eb718f4c64c0e4be8777785f03ca3710ea111
unknown <saadiqgp@gmail.com>
2024-04-12 13:50:56 -0400

    part 4 step one: fetched current user__

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,17 +1,17 @@
 import { createContext, useState } from "react";
 
-
-export const CurrentUser = createContext()
-
-function CurrentUserProvider({ children }){
-
-    const [currentUser, setCurrentUser] = useState(null)
-
-    return (
-        <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
-            {children}
-        </CurrentUser.Provider>
-    )
+function CurrentUserProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  useEffect(() => {
+    const getLoggedInUser = async () => {
+      let response = await fetch(
+        "http://localhost:5000/authentication/profile"
+      );
+      let user = await response.json();
+      setCurrentUser(user);
+    };
+    getLoggedInUser();
+  }, []);
 }
 
-export default CurrentUserProvider
+export default CurrentUserProvider;
